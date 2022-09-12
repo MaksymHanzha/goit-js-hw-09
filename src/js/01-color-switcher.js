5f2892ff7a9c3ee415aa0b020205e3096e962490
Maksym Hanzha <101345604+MaksymHanzha@users.noreply.github.com>
2022-09-12 22:01:18 +0300

    --1_TASK--
    Завдання зі зміною кольору фону

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let intervalId = null;
 
+btnStart.addEventListener('click', onBtnStart);
+btnStop.addEventListener('click', onBtnStop);
+
+function onBtnStart() {
+  //   console.log('start');
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStart.disabled = true;
+}
+
+function onBtnStop() {
+  //   console.log('stop');
+  clearInterval(intervalId);
+  btnStart.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
