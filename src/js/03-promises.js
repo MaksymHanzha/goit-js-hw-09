46de2cead462680cd598e246cd25039d2e35671c
Maksym Hanzha <101345604+MaksymHanzha@users.noreply.github.com>
2022-09-19 23:44:18 +0300

    --3_TASK--

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,36 +1,58 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const formEl = document.querySelector('.form');
-formEl.addEventListener('submit', onFormSubmit);
+const refs = {
+  form: document.querySelector('.form'),
+  fieldDelay: document.querySelector('input[name="delay"]'),
+  fieldStep: document.querySelector('input[name="step"]'),
+  fieldAmount: document.querySelector('input[name="amount"]'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  const { delay, step, amount } = getFormValues();
+
+  setTimeout(startPromises, delay, step, amount, delay);
+}
+
+function startPromises(step, amount, start) {
+  for (let i = 0; i < amount; i += 1) {
+    setTimeout(() => {
+      createPromise(i, step * i)
+        .then(({ position, delay }) => {
+          Notify.success(
+            `✅ Fulfilled promise ${position + 1} in ${delay + start}ms`
+          );
+        })
+        .catch(({ position, delay }) => {
+          Notify.failure(
+            `❌ Rejected promise ${position + 1} in ${delay + start}ms`
+          );
+        });
+    }, step);
+  }
+}
+
+function getFormValues() {
+  const values = {
+    delay: Number(refs.fieldDelay.value),
+    step: Number(refs.fieldStep.value),
+    amount: Number(refs.fieldAmount.value),
+  };
+
+  return values;
+}
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
   return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
       if (shouldResolve) {
         resolve({ position, delay });
-      } else {
-        reject({ position, delay });
       }
+
+      reject({ position, delay });
     }, delay);
   });
-}
-
-function onFormSubmit(e) {
-  e.preventDefault();
-
-  let delay = Number(e.target.elements.delay.value);
-  let step = Number(e.target.elements.step.value);
-  let amount = Number(e.target.elements.amount.value);
-
-  for (let i = 1; i <= amount; i += 1) {
-    createPromise(i, delay)
-      .then(({ position, delay }) => {
-        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      })
-      .catch(({ position, delay }) => {
-        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-      });
-    delay += step;
-  }
 }
